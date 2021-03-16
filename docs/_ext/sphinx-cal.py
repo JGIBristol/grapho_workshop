5ebae5a553e01ec55e9f1e37029a845814243174
Natalie Thurlby <natalie.thurlby@gmail.com>
2021-03-16 15:36:31 +0000

    _static

--- docs/_ext/sphinx-cal.py
+++ docs/_ext/sphinx-cal.py
@@ -93,7 +93,7 @@ def setup(app):
     )
     app.add_config_value(  # Where the events files are stored
         name="calendar_loc",
-        default="calendar.ics",
+        default="_static/calendar.ics",
         rebuild='',
     )
 
